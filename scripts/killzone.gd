3f799c6b77e068ac77671a399feea6106189b564
moaazkamel <moaaz.kamel.gber@gmail.com>
2026-04-22 20:35:14 +0200

    making a die animation when the player touch the enemy and make a background

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -5,9 +5,12 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print('YOU DIED')
+	Engine.time_scale = 0.5
+	body.get_node('CollisionShape2D').queue_free()
 	timer.start()
 	
 	
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
