7c72ae87e1eede9419e094a30016201e28ec3e01
moaazkamel <moaaz.kamel.gber@gmail.com>
2026-04-29 10:23:04 +0300

    enhance the level and make it very hard and edit one the player code

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,43 @@
 extends CharacterBody2D
 
 
-const SPEED = 90.0
-const JUMP_VELOCITY = -200.0
+var speed = 90.0
 
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
+var jump_velocity = -300.0
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var coyote_time = 0.15 
+
+var coyote_timer = 0.0
+
+
+
+
+func _physics_process(delta):
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed(" jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
+		velocity.y += gravity * delta
+		
+		
+		coyote_timer -= delta
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		coyote_timer = coyote_time
+
+
+
+
+
+	if Input.is_action_just_pressed("ui_accept") and coyote_timer > 0:
+		velocity.y = jump_velocity
+		coyote_timer = 0
+
+
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		
+		
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
